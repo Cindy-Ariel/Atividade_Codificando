b10690089ed9bcac0d4ea36b7d423ee33ad9fc13
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-05-19 16:19:36 -0300

    Criando os botões e área de texto

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-const cifraDeCesarCriptografar = (frases , deslocamento ) => {};
-const cifraDeCeDescriptografar = (palavra, minhaChave) => {};
+var tagBotao = document.getElementById("botaoOK");
+var tagInputEntrada = document.getElementById("entrada");
+var tagInputSaida = document.getElementById("saida");
+var botaoLimpar = document.getElementById("botaoLimpar");
 
-const _alfabeto = () => {
-  var alfabeto = new Map();
-  alfabeto.set(1,'a');
-  alfabeto.set(2,'b');
-  alfabeto.set(3,'c');
-  alfabeto.set(4,'d');
-  alfabeto.set(5,'e');
-  alfabeto.set(6,'f');
-  alfabeto.set(7,'g');
-  alfabeto.set(8,'h');
-  alfabeto.set(9,'i');
-  alfabeto.set(10,'j');
-  alfabeto.set(11,'k');
-  alfabeto.set(12,'l');
-  alfabeto.set(13,'m');
-  alfabeto.set(14,'n');
-  alfabeto.set(15,'o');
-  alfabeto.set(16,'p');
-  alfabeto.set(17,'q');
-  alfabeto.set(18,'r');
-  alfabeto.set(19,'s');
-  alfabeto.set(20,'t');
-  alfabeto.set(21,'u');
-  alfabeto.set(22,'v');
-  alfabeto.set(23,'w');
-  alfabeto.set(24,'x');
-  alfabeto.set(25,'y');
-  alfabeto.set(26,'z');
+function codificarCifraDeCesar(frase) {
+  var arrayDePalavras = frase.split(" ");
+  var novoArray = arrayDePalavras.map((palavra) => {
+    return palavra;
+  });
+  novoArray.join(" ");
 }
 
+tagBotao.addEventListener("click", (e) => {
+  e.preventDefault();
+  var valor = tagInputEntrada.value;
+  tagInputSaida.value = valor;
+  codificarCifraDeCesar(valor);
+});
+botaoLimpar.addEventListener("click", (e) => {
+  e.preventDefault();
+  tagInputEntrada.value = "";
+  tagInputSaida.value = "";
+});
+function rodarFormulario() {}
