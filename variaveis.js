0b65f99af4f98628f59ca3d2a270b9413391931b
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-05-19 11:15:01 -0300

    Initial commit

---
+++ variaveis.js
@@ -0,0 +1,8 @@
+var nome = Cindy 
+var idade = 24
+console.log ("O usuario de nome + "nome" + tem "idade" + anos")
+
+nome = Bruno 
+idade = 25
+
+console.log ("O usuario de nome + "nome" + tem "idade" + anos")
