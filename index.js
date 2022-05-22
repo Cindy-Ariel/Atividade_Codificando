065dde40738cd3a344b126157118930ce35aacb6
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-05-21 23:55:49 -0300

    Adionando Base64 e afins
    Ocultando campo de deslocamento caso Base64 seja selecionado, alterando frase do botão referente a opção selecionada.

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var tagInputSaida = document.getElementById("saida");
 var botaoLimpar = document.getElementById("botaoLimpar");
 var tagInputDeslocamento = document.getElementById("deslocamento");
 var botaoRadioCodificar = document.getElementById("codificar");
+var botaoRadioDecodificar = document.getElementById("decodificar");
 var seletor = document.getElementById("seletor");
 var option = seletor.options[seletor.selectedIndex];
+var deslocamento = document.getElementById("deslocamento");
 
 //Alfabeto que representa as letras com posicionamento real. (1 a 26)
 var arrayDeLetrasDoAlfabeto = [
@@ -44,47 +46,21 @@ var arrayDeLetrasDoAlfabeto = [
 
 //-Se o deslocamento for negativo ele recebe ele mesmo + 26 até que seja positivo, para assim representar uma posição de letra codificada real.
 
-function codificarLetraCC(letra) {
+//Para decodificar o deslocamento é multiplicado por -1 e a lógica é a mesma.
+
+function codificarEDecodificarLetraCC(letra, eCodificar) {
   var posicaoDessaLetra = arrayDeLetrasDoAlfabeto.indexOf(letra);
   var posicaoDaLetraCodificada = undefined;
-  if (parseInt(tagInputDeslocamento.value) > 0) {
-    var posicaoDaLetraCodificada =
-      parseInt(posicaoDessaLetra) + parseInt(tagInputDeslocamento.value);
+  let deslocamento = undefined;
+  if (eCodificar) {
+    deslocamento = parseInt(tagInputDeslocamento.value);
   } else {
-    var deslocamentoReal = parseInt(tagInputDeslocamento.value);
-    while (deslocamentoReal <= 0) {
-      deslocamentoReal = deslocamentoReal + 26;
-    }
-    var posicaoDaLetraCodificada =
-      parseInt(posicaoDessaLetra) + deslocamentoReal;
+    deslocamento = parseInt(tagInputDeslocamento.value) * -1;
   }
-
-  if (posicaoDaLetraCodificada <= 26) {
-    var letraCodificada = arrayDeLetrasDoAlfabeto[posicaoDaLetraCodificada];
-    return letraCodificada;
-  } else if (posicaoDaLetraCodificada > 26) {
-    while (posicaoDaLetraCodificada > 26) {
-      posicaoDaLetraCodificada = posicaoDaLetraCodificada - 26;
-    }
-    var letraCodificada = arrayDeLetrasDoAlfabeto[posicaoDaLetraCodificada];
-    return letraCodificada;
-  } else if (posicaoDaLetraCodificada <= 0) {
-    while (posicaoDaLetraCodificada <= 0) {
-      posicaoDaLetraCodificada = posicaoDaLetraCodificada + 26;
-    }
-    var letraCodificada = arrayDeLetrasDoAlfabeto[posicaoDaLetraCodificada];
-    return letraCodificada;
-  }
-}
-
-function decodificarLetraCC(letra) {
-  var posicaoDessaLetra = arrayDeLetrasDoAlfabeto.indexOf(letra);
-  var posicaoDaLetraCodificada = undefined;
-  var xdeslocamento = parseInt(tagInputDeslocamento.value) * -1;
-  if (xdeslocamento > 0) {
-    var posicaoDaLetraCodificada = parseInt(posicaoDessaLetra) + xdeslocamento;
+  if (deslocamento > 0) {
+    var posicaoDaLetraCodificada = parseInt(posicaoDessaLetra) + deslocamento;
   } else {
-    var deslocamentoReal = xdeslocamento;
+    var deslocamentoReal = deslocamento;
     while (deslocamentoReal <= 0) {
       deslocamentoReal = deslocamentoReal + 26;
     }
@@ -111,39 +87,51 @@ function decodificarLetraCC(letra) {
 }
 
 function tratarFrase(frase) {
-  var arrayDePalavras = frase.split(" "); // Divide a frase em palavras por espaço.
-  var arrayDePalavrasTratadas = arrayDePalavras.map((palavra) => {
-    var arrayDeLetras = palavra.split(""); // Divide as palavras em letras
-    var arrayDeLetrasTratadas = arrayDeLetras.map((letra) => {
-      let letraTratada = "";
-      // Se o Codificar está selecionado.
-      if (botaoRadioCodificar.checked) {
+  if (seletor.selectedIndex === 0) {
+    var arrayDePalavras = frase.split(" "); // Divide a frase em palavras por espaço.
+    var arrayDePalavrasTratadas = arrayDePalavras.map((palavra) => {
+      var arrayDeLetras = palavra.split(""); // Divide as palavras em letras
+      var arrayDeLetrasTratadas = arrayDeLetras.map((letra) => {
+        let letraTratada = "";
+        // Se for Cifra de Cesar.
         if (seletor.selectedIndex === 0) {
-          letraTratada = codificarLetraCC(letra);
-        } else {
-          letraTratada = codificarLetraB64(letra);
+          letraTratada = codificarEDecodificarLetraCC(
+            letra,
+            botaoRadioCodificar.checked
+          );
+          return letraTratada;
         }
-        // Se o Decodificar está selecionado.
-      } else {
-        if (seletor.selectedIndex === 0) {
-          letraTratada = decodificarLetraCC(letra);
-        } else {
-          letraTratada = decodificarLetraB64(letra);
-        }
-      }
-      return letraTratada;
+      });
+
+      //Junta as letras codificadas para formar palavras e junta as palavras para retornar a frase codificada.
+
+      var palavraTratada = arrayDeLetrasTratadas.join("");
+      return palavraTratada;
     });
+    var fraseTratada = arrayDePalavrasTratadas.join(" ");
+    return fraseTratada;
+  }
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //BASE 64
+  var fraseTratada = "";
+  if (botaoRadioCodificar.checked && seletor.selectedIndex === 1) {
+    fraseTratada = btoa(frase);
 
-    //Junta as letras codificadas para formar palavras e junta as palavras para retornar a frase codificada.
-
-    var palavraTratada = arrayDeLetrasTratadas.join("");
-    return palavraTratada;
-  });
-  var fraseTratada = arrayDePalavrasTratadas.join(" ");
+    // Se o Decodificar está selecionado e for Cifra de Cesar.
+  } else if (seletor.selectedIndex === 1) {
+    fraseTratada = atob(frase);
+  }
   return fraseTratada;
 }
 
-console.log(botaoRadioCodificar.checked);
+seletor.addEventListener("change", () => {
+  if (seletor.selectedIndex === 1) {
+    deslocamento.style.display = "none"; // Ocultar a div deslocamento se o seletor Base64 for selecionado.
+  } else {
+    deslocamento.style.display = "block"; // Retornar com a div deslomento se o Cifra de Cesar for selecionado.
+  }
+});
+
 //Ao clicar a frase é codificada e passada para o campo de saida de texto.
 tagBotao.addEventListener("click", (e) => {
   e.preventDefault();
@@ -152,13 +140,24 @@ tagBotao.addEventListener("click", (e) => {
   tagInputSaida.value = fraseTratada;
 });
 
+//Ao carregar a página o botão já apareça selecionado.
+window.addEventListener("load", () => {
+  botaoRadioCodificar.checked = true;
+});
+
+//Ao selecionar decodificar no botão a mensagem é alterada para Decodificar mensagem!
+botaoRadioDecodificar.addEventListener("click", () => {
+  tagBotao.innerHTML = "Decodificar mensagem!";
+});
+
+//Ao selecionar codificar no botão a mensagem é alterada para Codificar mensagem!
+botaoRadioCodificar.addEventListener("click", () => {
+  tagBotao.innerHTML = "Codificar mensagem!";
+});
+
 //Limpar os campos de texto ao clicar no botão.
 botaoLimpar.addEventListener("click", (e) => {
   e.preventDefault();
   tagInputEntrada.value = "";
   tagInputSaida.value = "";
 });
-
-function codificarLetraB64() {
-  console.log("ola");
-}
